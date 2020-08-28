5b9e7ae8f3d36dba268bd1ebeeb027dd494744ff
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-08-28 16:29:53 -0500

    updated models and index

--- models/index.js
+++ models/index.js
@@ -4,12 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id' 
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
